459fa568bd281c9a6f2f3fd56c757a4487caa215
lauradipascua-ctrl <laura.dipascua@anima.edu.uy>
2026-04-23 19:16:28 -0300

    ejercicio6fun.js

--- ejercicio6fun.js
+++ ejercicio6fun.js
@@ -13,19 +13,26 @@
 //return edad >= 18;
 //};
 //ejercicio 7 Explorar Math
-console.log(Math.round(7.5));
-console.log(Math.floor(7.9));
-console.log(Math.abs(-42));
-console.log(Math.max(1, 5, 3, 9, 2));
-console.log(Math.min(1, 5, 3, 9, 2));
-console.log(Math.pow(2, 8));
-console.log(Math.sqrt(144));
-console.log(Math.round(7.4));
-console.log(Math.ceil(7.1));
-console.log(Math.abs(24));
+// console.log(Math.round(7.5));
+// console.log(Math.floor(7.9));
+// console.log(Math.abs(-42));
+// console.log(Math.max(1, 5, 3, 9, 2));
+// console.log(Math.min(1, 5, 3, 9, 2));
+// console.log(Math.pow(2, 8));
+// console.log(Math.sqrt(144));
+// console.log(Math.round(7.4));
+// console.log(Math.ceil(7.1));
+// console.log(Math.abs(24));
 
 //ejercicio 8
 function tirarDado() {
-  return Math.round(Math.random() + 5);
+  return Math.floor(Math.random() * 6) + 1;
 }
 console.log(tirarDado());
+//ejercicio 9
+function areaCirculo(radio) {
+  return Math.PI * Math.pow(radio, 2);
+}
+console.log(areaCirculo(5));
+console.log(areaCirculo(10));
+//Ejercicio 10
