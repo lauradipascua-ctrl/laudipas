712cb48b3017eb0c1bd76d04929f522b167679e1
lauradipascua-ctrl <laura.dipascua@anima.edu.uy>
2026-04-23 18:35:17 -0300

    Funciones

---
+++ ejercicio6fun.js
@@ -0,0 +1,30 @@
+//function calcularDoble(n) {
+//return n * 2;
+//}
+
+//const calcularDoble = (n) => {
+//return n * 2;
+//};
+
+//function esMayorDeEdad(edad) {
+//return edad >= 18;
+//}
+//const esMayorDeEdad = (edad) => {
+//return edad >= 18;
+//};
+//ejercicio 7 Explorar Math
+console.log(Math.round(7.5));
+console.log(Math.floor(7.9));
+console.log(Math.abs(-42));
+console.log(Math.max(1, 5, 3, 9, 2));
+console.log(Math.min(1, 5, 3, 9, 2));
+console.log(Math.pow(2, 8));
+console.log(Math.sqrt(144));
+console.log(Math.round(7.4));
+console.log(Math.ceil(7.1));
+console.log(Math.abs(24));
+
+//ejercicio 8
+function tirarDado() {
+  return Math.round(Math.random() + 5);
+}
