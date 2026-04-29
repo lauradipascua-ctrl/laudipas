2214de4f0155cbf8ba72181b36b8aea1b5b218fd
lauradipascua-ctrl <laura.dipascua@anima.edu.uy>
2026-04-29 12:23:59 -0300

    ejercicio5

--- ejerciciosextrafun.js
+++ ejerciciosextrafun.js
@@ -42,3 +42,51 @@ function mayorDeTres(a, b, c) {
   return mayor;
 }
 console.log(mayorDeTres(3, 7, 2));
+//Ejercicio 5
+const caracteres = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+function gernerarContraseña(longitud) {
+  let contraseña = "";
+  for (let i = 0; i < longitud; i++) {
+    let indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+    contraseña = contraseña + caracteres[indiceAleatorio];
+  }
+  return contraseña;
+}
+console.log(gernerarContraseña(8));
