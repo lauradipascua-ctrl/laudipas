90b743038212bb28e81fd1d9a0da29ef3a4fb4c2
lauradipascua-ctrl <laura.dipascua@anima.edu.uy>
2026-04-30 19:31:22 -0300

    objetos

---
+++ objetosej1.js
@@ -0,0 +1,24 @@
+//Ejercicio1
+const cancion = {
+  titulo: "creo en mi",
+  artista: "Natalia Gimenez",
+  año: 2014,
+  duracion: 407,
+  favorita: true,
+};
+console.log(cancion);
+//Ejercicio2
+const libro = {
+  titulo: "Cien años de soledad",
+  autor: "Gabriel Garcia Marquez",
+  paginas: 471,
+  leido: false,
+};
+console.log(libro);
+//Ejercicio3
+const producto = {
+   nombre:"mouse vertical"
+   precio: 79,
+   stock: 4,
+   disponible: true,
+};
