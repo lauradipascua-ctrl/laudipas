dfb87ec04496966840853f559fe0d6e8fb59f13e
lauradipascua-ctrl <laura.dipascua@anima.edu.uy>
2026-05-08 22:02:42 -0300

    Update ejarrays.js

--- ejarrays.js
+++ ejarrays.js
@@ -20,4 +20,11 @@ peliculas[0].puntaje = 9.0;
 peliculas.push({"titanic": 2
 
   //Ejercici6
+   const peliculas = [ 
+    { titulo: "Toy Story",         anio: 1995, puntaje: 8.3 }, 
+    { titulo: "El Rey Leon",        anio: 1994, puntaje: 8.5 }, 
+    { titulo: "Eterno Resplandor",  anio: 2004, puntaje: 8.3 }, 
+    { titulo: "Parasite",           anio: 2019, puntaje: 8.6 }, 
+  ]; 
+
   
